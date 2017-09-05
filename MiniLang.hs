60308a2a5aea6aa3897697a290a6a9863180290b
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-09-05 15:56:53 +0200

    add some notes and bunch of extensions

--- MiniLang.hs
+++ MiniLang.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE DeriveDataTypeable, KindSignatures, TypeFamilies, DataKinds, PolyKinds, TypeInType #-}
+
 module MiniLang where
 
+import Data.Data
 import Test.Feat ( Enumerable, Enumerate
                  , enumerate, consts, unary, funcurry 
                  , values )
@@ -13,7 +16,23 @@ enumBounded :: (Enum a, Bounded a) => Enumerate a
 enumBounded = consts $ map pure [minBound..maxBound]
 
 
+type family
+  HasArg a (cons :: a -> b) :: Bool 
 
+{-
+This works in theory, when using :k in GHCI:
+
+λ> :k  HasArg GAP GAdjCN 
+HasArg GAP GAdjCN :: Bool      -- GAP is indeed an argument of CAdjCN, we get a Bool
+
+
+λ> :k  HasArg GAP GMassNP      -- GAP is not an argument of GMassNP, we get an error
+
+<interactive>:1:12: error:
+    • Expected kind ‘GAP -> GNP’, but ‘'GMassNP’ has kind ‘GCN -> GNP’
+    • In the second argument of ‘HasArg’, namely ‘GMassNP’
+      In the type ‘HasArg GAP GMassNP’
+-}
 
 
 ----------------------------------------------------
@@ -23,6 +42,7 @@ enumBounded = consts $ map pure [minBound..maxBound]
 class Gf a where
   gf :: a -> PGF.Tree
   fg :: PGF.Tree -> a
+  
 
 newtype GString = GString String  deriving Show
 
@@ -57,31 +77,11 @@ instance Gf GFloat where
 
 data GA =
    Gbad_A 
-{- | Gbig_A 
- | Gblack_A 
- | Gblue_A 
- | Gclean_A 
- | Gclever_A 
- | Gcold_A 
- | Gdirty_A 
- | Ggood_A 
- | Ggreen_A 
- | Gheavy_A 
- | Ghot_A 
- | Gnew_A 
- | Gold_A 
- | Gready_A 
- | Gred_A 
- | Gsmall_A 
- | Gwarm_A 
- | Gwhite_A 
- | Gyellow_A 
- | Gyoung_A -}
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GA where enumerate = enumBounded
 
 data GAP = GPositA GA 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GAP where
   enumerate = unary GPositA
 
@@ -89,7 +89,7 @@ data GAdv =
    GPrepNP GPrep GNP 
  | Galready_Adv 
  | Gnow_Adv 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GAdv where
   enumerate = consts ( unary (funcurry GPrepNP):
                        map pure [Galready_Adv,Gnow_Adv] 
@@ -100,74 +100,32 @@ instance Enumerable GAdv where
 data GCN =
    GAdjCN GAP GCN 
  | GUseN GN 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GCN where
   enumerate = consts [ unary (funcurry GAdjCN)
                      , unary GUseN
                      ]
 
 data GCl = GPredVP GNP GVP 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GCl where
   enumerate = consts [ unary (funcurry GPredVP) ]
 
 data GConj =
    Gand_Conj 
  | Gor_Conj 
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GConj where enumerate = enumBounded
 
 data GDet =
-   GaPl_Det 
- | Ga_Det 
- | Gevery_Det 
- | GthePl_Det 
+   GthePl_Det 
  | Gthe_Det 
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GDet where enumerate = enumBounded
 
 data GN =
    Ganimal_N 
-{- | Gapple_N 
- | Gbaby_N 
- | Gbeer_N 
- | Gbike_N 
- | Gbird_N 
- | Gblood_N 
- | Gboat_N 
- | Gbook_N 
- | Gboy_N 
- | Gbread_N 
- | Gcar_N 
- | Gcat_N 
- | Gchild_N 
- | Gcity_N 
- | Gcloud_N 
- | Gcomputer_N 
- | Gcow_N 
- | Gdog_N 
- | Gfire_N 
- | Gfish_N 
- | Gflower_N 
- | Gfriend_N 
- | Ggirl_N 
- | Ggrammar_N 
- | Ghorse_N 
- | Ghouse_N 
- | Glanguage_N 
- | Gman_N 
- | Gmilk_N 
- | Gmusic_N 
- | Griver_N 
- | Gsea_N 
- | Gship_N 
- | Gstar_N 
- | Gtrain_N 
- | Gtree_N 
- | Gwater_N 
- | Gwine_N 
- | Gwoman_N -}
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GN where enumerate = enumBounded
 
 data GNP =
@@ -175,7 +133,7 @@ data GNP =
  | GMassNP GCN 
  | GUsePN GPN 
  | GUsePron GPron 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GNP where
   enumerate = consts [ unary (funcurry GDetCN)
                      , unary GMassNP
@@ -185,38 +143,31 @@ instance Enumerable GNP where
 
 data GPN =
    Gjohn_PN 
- | Gparis_PN 
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GPN where enumerate = enumBounded
 
 data GPol =
    GPNeg 
  | GPPos 
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GPol where enumerate = enumBounded
 
 data GPrep =
    Gin_Prep 
- | Gon_Prep 
- | Gwith_Prep 
-  deriving (Show,Eq,Enum,Bounded)
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GPrep where enumerate = enumBounded  
 
 data GPron =
    Gi_Pron 
-{- | Ghe_Pron 
  | Gshe_Pron 
- | Gthey_Pron 
- | Gwe_Pron 
- | GyouPl_Pron 
- | GyouSg_Pron -}
-  deriving (Show,Eq,Enum,Bounded)
+ | GyouSg_Pron
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GPron where enumerate = enumBounded  
 
 data GS =
    GCoordS GConj GS GS 
  | GUsePresCl GPol GCl 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GS where
   enumerate = consts [ unary (funcurry GUsePresCl)
                      , unary (funcurry (funcurry GCoordS))
@@ -225,40 +176,20 @@ instance Enumerable GS where
 data GUtt =
    GUttNP GNP 
  | GUttS GS 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GUtt where
   enumerate = consts [ unary GUttNP
                      , unary GUttS
                      ]
 
 data GV =
-   Gcome_V 
- | Ggo_V 
- | Gjump_V 
- | Glive_V 
- | Gplay_V 
- | Grun_V 
- | Gsleep_V 
- | Gswim_V 
- | Gtravel_V 
- | Gwalk_V 
-  deriving (Show,Eq,Enum,Bounded)
+  Gsleep_V 
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GV where enumerate = enumBounded  
 
 data GV2 =
-   Gbreak_V2 
- | Gbuy_V2 
- | Gdrink_V2 
- | Geat_V2 
- | Gfind_V2 
- | Gkill_V2 
- | Glove_V2 
- | Gread_V2 
- | Gsee_V2 
- | Gteach_V2 
- | Gunderstand_V2 
- | Gwait_V2 
-  deriving (Show,Eq,Enum,Bounded)
+  Gsee_V2 
+  deriving (Show,Eq,Data,Typeable,Enum,Bounded)
 instance Enumerable GV2 where enumerate = enumBounded  
 
 data GVP =
@@ -266,7 +197,7 @@ data GVP =
  | GComplV2 GV2 GNP 
  | GUseAP GAP 
  | GUseV GV 
-  deriving (Show,Eq)
+  deriving (Show,Eq,Data,Typeable)
 instance Enumerable GVP where
   enumerate = consts [ unary GUseAP
                      , unary GUseV
@@ -276,52 +207,10 @@ instance Enumerable GVP where
 
 instance Gf GA where
   gf Gbad_A = mkApp (mkCId "bad_A") []
-{-  gf Gbig_A = mkApp (mkCId "big_A") []
-  gf Gblack_A = mkApp (mkCId "black_A") []
-  gf Gblue_A = mkApp (mkCId "blue_A") []
-  gf Gclean_A = mkApp (mkCId "clean_A") []
-  gf Gclever_A = mkApp (mkCId "clever_A") []
-  gf Gcold_A = mkApp (mkCId "cold_A") []
-  gf Gdirty_A = mkApp (mkCId "dirty_A") []
-  gf Ggood_A = mkApp (mkCId "good_A") []
-  gf Ggreen_A = mkApp (mkCId "green_A") []
-  gf Gheavy_A = mkApp (mkCId "heavy_A") []
-  gf Ghot_A = mkApp (mkCId "hot_A") []
-  gf Gnew_A = mkApp (mkCId "new_A") []
-  gf Gold_A = mkApp (mkCId "old_A") []
-  gf Gready_A = mkApp (mkCId "ready_A") []
-  gf Gred_A = mkApp (mkCId "red_A") []
-  gf Gsmall_A = mkApp (mkCId "small_A") []
-  gf Gwarm_A = mkApp (mkCId "warm_A") []
-  gf Gwhite_A = mkApp (mkCId "white_A") []
-  gf Gyellow_A = mkApp (mkCId "yellow_A") []
-  gf Gyoung_A = mkApp (mkCId "young_A") []-}
 
   fg t =
     case unApp t of
       Just (i,[]) | i == mkCId "bad_A" -> Gbad_A 
-{-      Just (i,[]) | i == mkCId "big_A" -> Gbig_A 
-      Just (i,[]) | i == mkCId "black_A" -> Gblack_A 
-      Just (i,[]) | i == mkCId "blue_A" -> Gblue_A 
-      Just (i,[]) | i == mkCId "clean_A" -> Gclean_A 
-      Just (i,[]) | i == mkCId "clever_A" -> Gclever_A 
-      Just (i,[]) | i == mkCId "cold_A" -> Gcold_A 
-      Just (i,[]) | i == mkCId "dirty_A" -> Gdirty_A 
-      Just (i,[]) | i == mkCId "good_A" -> Ggood_A 
-      Just (i,[]) | i == mkCId "green_A" -> Ggreen_A 
-      Just (i,[]) | i == mkCId "heavy_A" -> Gheavy_A 
-      Just (i,[]) | i == mkCId "hot_A" -> Ghot_A 
-      Just (i,[]) | i == mkCId "new_A" -> Gnew_A 
-      Just (i,[]) | i == mkCId "old_A" -> Gold_A 
-      Just (i,[]) | i == mkCId "ready_A" -> Gready_A 
-      Just (i,[]) | i == mkCId "red_A" -> Gred_A 
-      Just (i,[]) | i == mkCId "small_A" -> Gsmall_A 
-      Just (i,[]) | i == mkCId "warm_A" -> Gwarm_A 
-      Just (i,[]) | i == mkCId "white_A" -> Gwhite_A 
-      Just (i,[]) | i == mkCId "yellow_A" -> Gyellow_A 
-      Just (i,[]) | i == mkCId "young_A" -> Gyoung_A -}
-
-
       _ -> error ("no A " ++ show t)
 
 instance Gf GAP where
@@ -383,17 +272,11 @@ instance Gf GConj where
       _ -> error ("no Conj " ++ show t)
 
 instance Gf GDet where
-  gf GaPl_Det = mkApp (mkCId "aPl_Det") []
-  gf Ga_Det = mkApp (mkCId "a_Det") []
-  gf Gevery_Det = mkApp (mkCId "every_Det") []
   gf GthePl_Det = mkApp (mkCId "thePl_Det") []
   gf Gthe_Det = mkApp (mkCId "the_Det") []
 
   fg t =
     case unApp t of
-      Just (i,[]) | i == mkCId "aPl_Det" -> GaPl_Det 
-      Just (i,[]) | i == mkCId "a_Det" -> Ga_Det 
-      Just (i,[]) | i == mkCId "every_Det" -> Gevery_Det 
       Just (i,[]) | i == mkCId "thePl_Det" -> GthePl_Det 
       Just (i,[]) | i == mkCId "the_Det" -> Gthe_Det 
 
@@ -402,90 +285,10 @@ instance Gf GDet where
 
 instance Gf GN where
   gf Ganimal_N = mkApp (mkCId "animal_N") []
- {- gf Gapple_N = mkApp (mkCId "apple_N") []
-  gf Gbaby_N = mkApp (mkCId "baby_N") []
-  gf Gbeer_N = mkApp (mkCId "beer_N") []
-  gf Gbike_N = mkApp (mkCId "bike_N") []
-  gf Gbird_N = mkApp (mkCId "bird_N") []
-  gf Gblood_N = mkApp (mkCId "blood_N") []
-  gf Gboat_N = mkApp (mkCId "boat_N") []
-  gf Gbook_N = mkApp (mkCId "book_N") []
-  gf Gboy_N = mkApp (mkCId "boy_N") []
-  gf Gbread_N = mkApp (mkCId "bread_N") []
-  gf Gcar_N = mkApp (mkCId "car_N") []
-  gf Gcat_N = mkApp (mkCId "cat_N") []
-  gf Gchild_N = mkApp (mkCId "child_N") []
-  gf Gcity_N = mkApp (mkCId "city_N") []
-  gf Gcloud_N = mkApp (mkCId "cloud_N") []
-  gf Gcomputer_N = mkApp (mkCId "computer_N") []
-  gf Gcow_N = mkApp (mkCId "cow_N") []
-  gf Gdog_N = mkApp (mkCId "dog_N") []
-  gf Gfire_N = mkApp (mkCId "fire_N") []
-  gf Gfish_N = mkApp (mkCId "fish_N") []
-  gf Gflower_N = mkApp (mkCId "flower_N") []
-  gf Gfriend_N = mkApp (mkCId "friend_N") []
-  gf Ggirl_N = mkApp (mkCId "girl_N") []
-  gf Ggrammar_N = mkApp (mkCId "grammar_N") []
-  gf Ghorse_N = mkApp (mkCId "horse_N") []
-  gf Ghouse_N = mkApp (mkCId "house_N") []
-  gf Glanguage_N = mkApp (mkCId "language_N") []
-  gf Gman_N = mkApp (mkCId "man_N") []
-  gf Gmilk_N = mkApp (mkCId "milk_N") []
-  gf Gmusic_N = mkApp (mkCId "music_N") []
-  gf Griver_N = mkApp (mkCId "river_N") []
-  gf Gsea_N = mkApp (mkCId "sea_N") []
-  gf Gship_N = mkApp (mkCId "ship_N") []
-  gf Gstar_N = mkApp (mkCId "star_N") []
-  gf Gtrain_N = mkApp (mkCId "train_N") []
-  gf Gtree_N = mkApp (mkCId "tree_N") []
-  gf Gwater_N = mkApp (mkCId "water_N") []
-  gf Gwine_N = mkApp (mkCId "wine_N") []
-  gf Gwoman_N = mkApp (mkCId "woman_N") [] -}
 
   fg t =
     case unApp t of
-{-      Just (i,[]) | i == mkCId "animal_N" -> Ganimal_N 
-      Just (i,[]) | i == mkCId "apple_N" -> Gapple_N 
-      Just (i,[]) | i == mkCId "baby_N" -> Gbaby_N 
-      Just (i,[]) | i == mkCId "beer_N" -> Gbeer_N 
-      Just (i,[]) | i == mkCId "bike_N" -> Gbike_N 
-      Just (i,[]) | i == mkCId "bird_N" -> Gbird_N 
-      Just (i,[]) | i == mkCId "blood_N" -> Gblood_N 
-      Just (i,[]) | i == mkCId "boat_N" -> Gboat_N 
-      Just (i,[]) | i == mkCId "book_N" -> Gbook_N 
-      Just (i,[]) | i == mkCId "boy_N" -> Gboy_N 
-      Just (i,[]) | i == mkCId "bread_N" -> Gbread_N 
-      Just (i,[]) | i == mkCId "car_N" -> Gcar_N 
-      Just (i,[]) | i == mkCId "cat_N" -> Gcat_N 
-      Just (i,[]) | i == mkCId "child_N" -> Gchild_N 
-      Just (i,[]) | i == mkCId "city_N" -> Gcity_N 
-      Just (i,[]) | i == mkCId "cloud_N" -> Gcloud_N 
-      Just (i,[]) | i == mkCId "computer_N" -> Gcomputer_N 
-      Just (i,[]) | i == mkCId "cow_N" -> Gcow_N 
-      Just (i,[]) | i == mkCId "dog_N" -> Gdog_N 
-      Just (i,[]) | i == mkCId "fire_N" -> Gfire_N 
-      Just (i,[]) | i == mkCId "fish_N" -> Gfish_N 
-      Just (i,[]) | i == mkCId "flower_N" -> Gflower_N 
-      Just (i,[]) | i == mkCId "friend_N" -> Gfriend_N 
-      Just (i,[]) | i == mkCId "girl_N" -> Ggirl_N 
-      Just (i,[]) | i == mkCId "grammar_N" -> Ggrammar_N 
-      Just (i,[]) | i == mkCId "horse_N" -> Ghorse_N 
-      Just (i,[]) | i == mkCId "house_N" -> Ghouse_N 
-      Just (i,[]) | i == mkCId "language_N" -> Glanguage_N 
-      Just (i,[]) | i == mkCId "man_N" -> Gman_N 
-      Just (i,[]) | i == mkCId "milk_N" -> Gmilk_N 
-      Just (i,[]) | i == mkCId "music_N" -> Gmusic_N 
-      Just (i,[]) | i == mkCId "river_N" -> Griver_N 
-      Just (i,[]) | i == mkCId "sea_N" -> Gsea_N 
-      Just (i,[]) | i == mkCId "ship_N" -> Gship_N 
-      Just (i,[]) | i == mkCId "star_N" -> Gstar_N 
-      Just (i,[]) | i == mkCId "train_N" -> Gtrain_N 
-      Just (i,[]) | i == mkCId "tree_N" -> Gtree_N 
-      Just (i,[]) | i == mkCId "water_N" -> Gwater_N 
-      Just (i,[]) | i == mkCId "wine_N" -> Gwine_N 
-      Just (i,[]) | i == mkCId "woman_N" -> Gwoman_N -}
-
-
+      Just (i,[]) | i == mkCId "animal_N" -> Ganimal_N 
       _ -> error ("no N " ++ show t)
 
 instance Gf GNP where
@@ -506,14 +309,10 @@ instance Gf GNP where
 
 instance Gf GPN where
   gf Gjohn_PN = mkApp (mkCId "john_PN") []
-  gf Gparis_PN = mkApp (mkCId "paris_PN") []
 
   fg t =
     case unApp t of
       Just (i,[]) | i == mkCId "john_PN" -> Gjohn_PN 
-      Just (i,[]) | i == mkCId "paris_PN" -> Gparis_PN 
-
-
       _ -> error ("no PN " ++ show t)
 
 instance Gf GPol where
@@ -530,38 +329,20 @@ instance Gf GPol where
 
 instance Gf GPrep where
   gf Gin_Prep = mkApp (mkCId "in_Prep") []
-  gf Gon_Prep = mkApp (mkCId "on_Prep") []
-  gf Gwith_Prep = mkApp (mkCId "with_Prep") []
 
   fg t =
     case unApp t of
       Just (i,[]) | i == mkCId "in_Prep" -> Gin_Prep 
-      Just (i,[]) | i == mkCId "on_Prep" -> Gon_Prep 
-      Just (i,[]) | i == mkCId "with_Prep" -> Gwith_Prep 
-
-
       _ -> error ("no Prep " ++ show t)
 
 instance Gf GPron where
   gf Gi_Pron = mkApp (mkCId "i_Pron") []
-  --gf Ghe_Pron = mkApp (mkCId "he_Pron") []
-  --gf Gshe_Pron = mkApp (mkCId "she_Pron") []
-  --gf Gthey_Pron = mkApp (mkCId "they_Pron") []
-  --gf Gwe_Pron = mkApp (mkCId "we_Pron") []
-  --gf GyouPl_Pron = mkApp (mkCId "youPl_Pron") []
-  --gf GyouSg_Pron = mkApp (mkCId "youSg_Pron") []
+  gf Gshe_Pron = mkApp (mkCId "she_Pron") []
 
   fg t =
     case unApp t of
       Just (i,[]) | i == mkCId "i_Pron" -> Gi_Pron 
-      --Just (i,[]) | i == mkCId "he_Pron" -> Ghe_Pron 
-      --Just (i,[]) | i == mkCId "she_Pron" -> Gshe_Pron 
-      --Just (i,[]) | i == mkCId "they_Pron" -> Gthey_Pron 
-      --Just (i,[]) | i == mkCId "we_Pron" -> Gwe_Pron 
-      --Just (i,[]) | i == mkCId "youPl_Pron" -> GyouPl_Pron 
-      --Just (i,[]) | i == mkCId "youSg_Pron" -> GyouSg_Pron 
-
-
+      Just (i,[]) | i == mkCId "she_Pron" -> Gshe_Pron 
       _ -> error ("no Pron " ++ show t)
 
 instance Gf GS where
@@ -589,63 +370,19 @@ instance Gf GUtt where
       _ -> error ("no Utt " ++ show t)
 
 instance Gf GV where
-  gf Gcome_V = mkApp (mkCId "come_V") []
-  gf Ggo_V = mkApp (mkCId "go_V") []
-  gf Gjump_V = mkApp (mkCId "jump_V") []
-  gf Glive_V = mkApp (mkCId "live_V") []
-  gf Gplay_V = mkApp (mkCId "play_V") []
-  gf Grun_V = mkApp (mkCId "run_V") []
   gf Gsleep_V = mkApp (mkCId "sleep_V") []
-  gf Gswim_V = mkApp (mkCId "swim_V") []
-  gf Gtravel_V = mkApp (mkCId "travel_V") []
-  gf Gwalk_V = mkApp (mkCId "walk_V") []
 
   fg t =
     case unApp t of
-      Just (i,[]) | i == mkCId "come_V" -> Gcome_V 
-      Just (i,[]) | i == mkCId "go_V" -> Ggo_V 
-      Just (i,[]) | i == mkCId "jump_V" -> Gjump_V 
-      Just (i,[]) | i == mkCId "live_V" -> Glive_V 
-      Just (i,[]) | i == mkCId "play_V" -> Gplay_V 
-      Just (i,[]) | i == mkCId "run_V" -> Grun_V 
       Just (i,[]) | i == mkCId "sleep_V" -> Gsleep_V 
-      Just (i,[]) | i == mkCId "swim_V" -> Gswim_V 
-      Just (i,[]) | i == mkCId "travel_V" -> Gtravel_V 
-      Just (i,[]) | i == mkCId "walk_V" -> Gwalk_V 
-
-
       _ -> error ("no V " ++ show t)
 
 instance Gf GV2 where
-  gf Gbreak_V2 = mkApp (mkCId "break_V2") []
-  gf Gbuy_V2 = mkApp (mkCId "buy_V2") []
-  gf Gdrink_V2 = mkApp (mkCId "drink_V2") []
-  gf Geat_V2 = mkApp (mkCId "eat_V2") []
-  gf Gfind_V2 = mkApp (mkCId "find_V2") []
-  gf Gkill_V2 = mkApp (mkCId "kill_V2") []
-  gf Glove_V2 = mkApp (mkCId "love_V2") []
-  gf Gread_V2 = mkApp (mkCId "read_V2") []
   gf Gsee_V2 = mkApp (mkCId "see_V2") []
-  gf Gteach_V2 = mkApp (mkCId "teach_V2") []
-  gf Gunderstand_V2 = mkApp (mkCId "understand_V2") []
-  gf Gwait_V2 = mkApp (mkCId "wait_V2") []
 
   fg t =
     case unApp t of
-      Just (i,[]) | i == mkCId "break_V2" -> Gbreak_V2 
-      Just (i,[]) | i == mkCId "buy_V2" -> Gbuy_V2 
-      Just (i,[]) | i == mkCId "drink_V2" -> Gdrink_V2 
-      Just (i,[]) | i == mkCId "eat_V2" -> Geat_V2 
-      Just (i,[]) | i == mkCId "find_V2" -> Gfind_V2 
-      Just (i,[]) | i == mkCId "kill_V2" -> Gkill_V2 
-      Just (i,[]) | i == mkCId "love_V2" -> Glove_V2 
-      Just (i,[]) | i == mkCId "read_V2" -> Gread_V2 
       Just (i,[]) | i == mkCId "see_V2" -> Gsee_V2 
-      Just (i,[]) | i == mkCId "teach_V2" -> Gteach_V2 
-      Just (i,[]) | i == mkCId "understand_V2" -> Gunderstand_V2 
-      Just (i,[]) | i == mkCId "wait_V2" -> Gwait_V2 
-
-
       _ -> error ("no V2 " ++ show t)
 
 instance Gf GVP where
