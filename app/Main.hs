20773cee041c01d4c40936d7b086d95b40db3948
Inari Listenmaa <inari.listenmaa@gmail.com>
2017-11-10 18:39:09 +0000

    some changes

--- app/Main.hs
+++ app/Main.hs
@@ -12,65 +12,21 @@ import Data.Maybe
 
 main :: IO ()
 main = do
-  grName <- getDataFileName "TestLang.pgf" 
+--  grName <- getDataFileName "TestLang.pgf" 
+  grName <- getDataFileName "Phrasebook.pgf" 
+  gr <- readGrammar grName
 
---  grName <- getDataFileName "Phrasebook.pgf" 
-  gr <- readGrammar grName  
---  mapM_ (print.(\(x,y,z,_) -> (x,y,z))) $ concrCats gr
-
---  mapM_ print (sort $ swapLinFuns gr) -- Not a good idea for languages with 10k+ categories >_>
 --  mapM_ print (productions gr `map` [100..120])
+--  print $ coercions gr
+
+  let trees = treesByCCat gr
+  mapM_ print $ sort trees         -- with foldl
+--  mapM_ (mapM_ print.sort) trees   -- with scanl
+  print $ length trees
+
+
   putStrLn "---"
-  let zeroArgTrees = same2 
-       [ (ccat, App symbol []) 
-        | (ccat,(fun,[])) <- swapLinFuns gr
-        , let cat = getCat ccat
-        , let symbol = Symbol fun ([],cat) [([],ccat)] ] --TODO 
-
-  --use scanl to find out how many trees you get at each step
-  let trees = nub $ foldl findTrees zeroArgTrees (take 10 $ repeat gr)
-  --mapM_ print $ sort trees
-  --print $ length $ swapLinFuns gr
-  --print $ length trees
-  --print $ coercions gr
-
-
-  --putStrLn "END"
-
   args <- getArgs
   case args of 
     ("all":_) -> mapM_ (assertLin gr) (filter hasArg $ symbols gr)
     (funNm:_) -> assertLin gr (lookupSymbol gr funNm)
-
- where
-  same2 :: (Eq a) => [(a,b)] -> [(a,b)] 
-  same2 = nubBy (\(a,_) (b,_) -> a==b) 
-
-  same3 :: (Eq a) => [(a,b,c)] -> [(a,b,c)] 
-  same3 = nubBy (\(a,_,_) (b,_,_) -> a==b)
-
-  findFuns :: (Eq a) => [a] -> [(a,b)] -> [b]
-  findFuns args examples = 
-   let funs = map (`lookup` examples) args 
-   in if all isJust funs then map fromJust funs else []
-
-  mkTree :: Symbol -> Tree
-  mkTree s = App s []
-
-  getCat :: ConcrCat -> Cat
-  getCat (CC (Just c) _) = c
-  getCat (CC Nothing _) = "TODO" --TODO: coercions
-
-  swapLinFuns :: Grammar -> [(ConcrCat,(Name,[ConcrCat]))]
-  swapLinFuns gr = [ (resCcat,(fun,argCcats))  
-                    | (Symbol fun _args ccats) <- symbols gr
-                    , (argCcats,resCcat) <- ccats ]
-
-  findTrees :: [(ConcrCat,Tree)] -> Grammar -> [(ConcrCat,Tree)]
-  findTrees nArgTrees gr
-    = nArgTrees ++ [ ( resCcat, App s foundTrees ) 
-         | s@(Symbol f t ccats) <- symbols gr
-         , (argCcats,resCcat) <- ccats
-         , let foundTrees = findFuns argCcats nArgTrees :: [Tree]
-         , not (null foundTrees) 
-      ] 
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@ module Lib
     , hasArg
     , lookupSymbol
     , bestExamples
+    , treesByCCat
     ) where
 
 import GrammarC
@@ -20,7 +21,7 @@ assertLin gr fun = do
   pr (treesUsingFun, map (linearize gr) treesUsingFun)
  where
   pr ([],[]) = putStrLn ""
-  pr (t:ts,l:ls) = do putStrLn (show t ++ " : " ++ snd (typ $ top t))
+  pr (t:ts,l:ls) = do putStrLn (show t ++ " : " ++ concatMap (show.snd) (concrTypes $ top t))
                       print l 
                       putStrLn ""
                       pr (ts,ls)
@@ -32,6 +33,49 @@ lookupSymbol gr str =
  where
   symb2table s@(Symbol nm _ _) = (nm,s)
   funsWithArgs = filter hasArg $ symbols gr
+
+--------------------------------------------------------------------------------
+-- Method 1, without FEAT. 
+-- Slow but gives at least some trees for more complicated grammars.
+
+--treesByCCat :: Grammar -> [(ConcrCat,Tree)]
+treesByCCat gr = bestTrees $ foldl findTrees zeroArgTrees (replicate 10 gr)
+  --use scanl to find out how many trees you get at each step
+
+ where
+
+  findTrees :: [(ConcrCat,Tree)] -> Grammar -> [(ConcrCat,Tree)]
+  findTrees nArgTrees gr
+    = nArgTrees ++ [ ( resCcat, App s bestArgs ) 
+         | s@(Symbol f t ccats) <- symbols gr
+         , (argCcats,resCcat) <- ccats
+         , let lookup' ccat = [ (cat,tree) | (cat,tree) <- nArgTrees, cat==ccat ]
+         , let argsOfCcat = map lookup' argCcats
+         , all (not.null) argsOfCcat -- if >1 of the argCcats is not in nArgTrees, break
+--         , let bestArgs = map (snd.head) argsOfCcat ]
+         , let bestArgs = map (snd.bestTree) argsOfCcat ]
+
+  concrFuns :: Grammar -> [(ConcrCat,(Name,[ConcrCat]))]
+  concrFuns gr = [ (resCcat,(fun,argCcats))  
+                  | (Symbol fun _args ccats) <- symbols gr
+                  , (argCcats,resCcat) <- ccats ]
+
+  zeroArgTrees = bestTrees
+    [ (ccat, App symbol []) 
+      | ( ccat@(CC (Just cat) _)     -- Assuming no coercions -- TODO is this correct?
+        , (fun,[]) ) <- concrFuns gr -- Match only trees with 0 arguments
+      , let symbol = Symbol fun ([],cat) [([],ccat)] ]
+
+  bestTrees :: [(ConcrCat,Tree)] -> [(ConcrCat,Tree)]
+  bestTrees = map bestTree . groupBy (\(a,_) (b,_) -> a==b) . sort
+
+  bestTree :: [(ConcrCat,Tree)] -> (ConcrCat,Tree)
+  bestTree ccats_trees = (head ccats, head $ bestExamples gr trees)
+   where (ccats,trees) = unzip ccats_trees
+
+
+--------------------------------------------------------------------------------
+-- Method 2, with FEAT. Doesn't run at all for e.g. Basque or Estonian.
 
 
 nextLevel :: Grammar -> Symbol -> [[Tree]] -- a list for each concrete category
@@ -61,19 +105,6 @@ nextLevel gr origF = concat trees
 
 --------------------------------------------------------------------------------
 
---representativeTrees :: Int -> Grammar -> ConcrCat -> [Tree]
---representativeTrees i gr cat = trace ("repTrees: " ++ show trees) $ concat trees
--- where
---  trees = [ [ tree | am <- [0..amount-1] 
---                   , let tree = featIth gr cat size am
---                   , norepeat tree ] 
---            | (size,amount) <- take i sz_am ] -- Take i first sizes that exist
-
---  -- Try all sizes from 1 to 100, keep ones that exist & how many there are
---  sz_am = [ (size,amount) | size <- [1..100]
---                          , let amount = featCard gr cat size 
---                          , amount > 0 ]
-
 norepeat :: Tree -> Bool
 norepeat t = let tops = collapse t in nub tops == tops
 
@@ -87,11 +118,8 @@ defaultTrees gr c = case nonEmptySizes of
   [] -> []
   cs -> bestExamples gr
           [ featIth gr c size card 
-              
             | (size,cd) <- cs
             , card <- [0..cd-1] ]
-                  
-
  where
   nonEmptySizes = [ (size,card) | size <- [1..5]
                                 , let card = featCard gr c size 
@@ -105,6 +133,7 @@ hasArg s = case s of
   Symbol _ ([], _) _ -> False
   _                  -> True
 
+
 testsAsWellAs :: (Eq a, Eq b) => [a] -> [b] -> Bool
 xs `testsAsWellAs` ys = go (xs `zip` ys)
  where
@@ -116,39 +145,18 @@ xs `testsAsWellAs` ys = go (xs `zip` ys)
     go [ xy | xy@(x',_) <- xys, x /= x' ]
 
 
-
---bestExamples :: Grammar -> [Tree] -> [Tree]
+bestExamples :: Grammar -> [Tree] -> [Tree]
 bestExamples gr [] = []
-bestExamples gr trees = take 1 $ map fst $ sortBy goodTest trees_lins
-
+bestExamples gr trees = take 1 $ map fst $ sortBy (goodTest `mappend` shorterTree) trees_lins
 
  where
   trees_lins = [ (tree,map snd (tabularLin gr tree)) | tree <- trees ]
   goodTest :: (Eq a) => (b,[a]) -> (b,[a]) -> Ordering
-  goodTest x y = if testsAsWellAs (snd x) (snd y) then GT else LT
-  --tabLins = map (snd . tabularLin gr) trees :: [[String]] -- one list for one tree
+  goodTest x y = if testsAsWellAs (snd x) (snd y) then LT else GT
 
-  --indexedTabLins = [ go tl [] | tl <- tabLins ] :: [[(Int,(String,String))]]
+  shorterTree :: (Tree,[a]) -> (Tree,[a]) -> Ordering
+  shorterTree x y = length (collapse $ fst x) `compare` length (collapse $ fst y)
 
-  --diff tabLins = maximum (map fst tabLins)
-  --maxDiff = maximum (map diff indexedTabLins)
-
-  --go :: [(String,String)] -> [(Int,(String,String))] -> [(Int,(String,String))]
-  --go []           res = res -- :: [((String,String),Int)]
-  --go (tl:tabLins) []  = go tabLins [(1,tl)]
-  --go (tl:tabLins) res = 
-  --  case lookup (snd tl) [ (s,i) | (i,(f,s)) <- res ] of
-  --    Just ind -> go tabLins $ (ind,tl):res
-  --    Nothing -> let largestInd = maximum (map fst res)
-  --                in go tabLins $ (largestInd+1,tl):res
-
---map fst $
-  --nubBy (\(_,tl1) (_,tl2) -> map snd tl1 == map snd tl2) -- e.g. #1,#2,#2 and #1,#1,#2
-  --  [ (t,tl) | (t,tl) <- zip trees indexedTabLins :: [(Tree, [(Int,(String,String))] )]
-  --           , diff tl == maxDiff ]
-
-   -- Debug version
---   sort [ tl | (t,tl) <- zip trees indexedTabLins ] 
 
 isSubtree :: Tree -> Tree -> Bool
 isSubtree t1 t2 = t1 `elem` args t2
