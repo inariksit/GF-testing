569ea95f32f87dcf1b3b5ea05c938e9b97eac4e9
Inari Listenmaa <inari.listenmaa@gmail.com>
2018-01-16 16:24:05 +0100

    add a function that tests the concrete cats of two grammars

--- src/GrammarC.hs
+++ src/GrammarC.hs
@@ -401,4 +401,18 @@ mkFEAT gr = catList
     (n',h') = parts nhs
 
 --------------------------------------------------------------------------------
+-- compare two grammars
+
+diffCats :: Grammar -> Grammar -> [(Cat,[Int],[String],[String])]
+diffCats gr1 gr2 = 
+  [ (acat1,[difFid c1, difFid c2],labels1  \\ labels2,labels2 \\ labels1)
+    | c1@(acat1,sfid1,efid1,labels1) <- concrCats gr1
+    , c2@(acat2,sfid2,efid2,labels2) <- concrCats gr2
+    , difFid c1 /= difFid c2 -- different amount of concrete categories
+      || labels1 /= labels2 -- or the labels are different
+    , acat1==acat2 ]
+
+ where
+  difFid (_,i,j,_) = 1 + (j-i)
+
 
