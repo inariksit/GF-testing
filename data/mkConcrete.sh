6afc33d36084ce158366323a6876ba368843ef63
Inari Listenmaa <inari.listenmaa@gmail.com>
2018-01-18 17:16:36 +0100

    make it compile all languages in /grammars

--- data/mkConcrete.sh
+++ data/mkConcrete.sh
@@ -4,4 +4,4 @@ LANG=$1
 
 GRAMMAR="TestLang$LANG"
 echo "concrete $GRAMMAR of TestLang = Grammar$LANG, Lexicon$LANG ;" > grammars/$GRAMMAR.gf
-gf -make --src -gfo-dir /tmp grammars/$GRAMMAR.gf
+gf -make --src -gfo-dir /tmp grammars/*.gf
