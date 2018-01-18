f60557fff6a0cb293556877e5c675db8b9c361ae
Inari Listenmaa <inari.listenmaa@gmail.com>
2018-01-18 17:17:20 +0100

    cleanup & use two languages

--- app/Main.hs
+++ app/Main.hs
@@ -5,8 +5,8 @@ import Lib
 import Paths_GF_testing
 import System.Environment
 
-import Data.List
-import Data.Maybe
+import Control.Monad ( when )
+import Data.List ( intercalate )
 
 import System.IO
 
@@ -18,43 +18,49 @@ main = do
 
   let debug = "d" `elem` args
   let compareWithOld = "cwo" `elem` args
+  let useTreebank = "tb" `elem` args
+
+  let concName = "TestLangDut" --TODO: user input
+  let concTrans = "TestLangEng" 
 
   grName <- getDataFileName "TestLang.pgf" 
-  gr <- readGrammar grName
-  grOld <- if compareWithOld
-             then readGrammar =<< getDataFileName "TestLangOld.pgf"
-             else return gr
-
+  gr     <- readGrammar concName grName
+  grTrans <- readGrammar concTrans grName
 
 
   case args of 
 
+    ("parse":_) -> mapM_ print $ parse gr "ik ben een hond"
+
     ("all":_) -> sequence_ $ take 5
-      [ testFun debug gr (show symb)
-         | symb <- symbols gr ]
+      [ testFun debug gr [] (show symb)
+        | symb <- symbols gr ]
 
-
-
-    (detCN:_) -> testFun debug gr detCN
+    (detCN:_) -> testFun debug gr [grTrans] detCN
 
     _ -> sequence_ 
         [ do putStrLn (showConcrFun gr symb)
           | symb <- symbols gr ]
 
-  if compareWithOld
-   then sequence_ [ do putStrLn $ "### " ++ acat
-                       putStr "Compiles to "
-                       putStr (show nOld)
-                       putStr " concrete categories in old grammar, "
-                       putStr (show nNew)
-                       putStrLn " in the new grammar.  "
-                       putStr "* Labels only in old: "
-                       putStrLn $ intercalate ", " labelsOld
-                       putStr "* Labels only in new: "
-                       putStrLn $ intercalate ", " labelsNew
-                       putStrLn ""
+-------------------------------------------------------------------------------
+-- secondary operations: read trees from treebank, compare with old grammar
 
-                    | (acat, [nOld,nNew], labelsOld, labelsNew) <- diffCats grOld gr ]
-     else return ()
+  when useTreebank $ do 
+    treebank <- lines `fmap` (readFile =<< getDataFileName "treebank.txt")
+    sequence_ [ do let tree = readTree gr str
+                   mapM_ putStrLn $ tabularPrint gr tree
+                | str <- treebank ]
+
+
+  when compareWithOld $ do
+    grOld <- readGrammar concName =<< getDataFileName "TestLangOld.pgf"
+    sequence_
+        [ putStrLn $ unlines $ 
+           [ "### " ++ acat
+           , (show nOld) ++ " concrete categories in the old grammar, "
+           , (show nNew) ++ " concrete categories in the new grammar.  "
+           , "* Labels only in old: " ++ intercalate ", " labelsOld
+           , "* Labels only in new: " ++ intercalate ", " labelsNew ]
+        | (acat, [nOld,nNew], labelsOld, labelsNew) <- diffCats grOld gr ]
 
 
